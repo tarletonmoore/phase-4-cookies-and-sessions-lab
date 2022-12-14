d055986a5dd3cb307b0eec4ce47032b9ed125011
John Tarleton Moore <tarletonmoore@gmail.com>
2022-12-14 14:43:08 -0500

    commit tree 8055ad7613c8dd82662ed5e89bc8a1e36e9c6762

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,14 +7,28 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    cookies[:page_views] ||= 0
+    cookies[:page_views] = cookies[:page_views].to_i + 1
+    session[:page_views] ||= 0
+    session[:page_views] += 1
     article = Article.find(params[:id])
+    if cookies[:page_views] > 3
+      limit_reached
+    else
     render json: article
+    end
   end
+
+
 
   private
 
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
+  end
+
+  def limit_reached
+    render json: { error: "Maximum pageview limit reached"}, status: :unauthorized
   end
 
 end
